f8fed20ab9fd2df6ac4c615258363f4025cbf97b
Neil Hyde <neil.hyde17@gmail.com>
2023-02-22 18:29:19 -0600

    Added model for Card and DeckOfCards

---
+++ Models/Card.cs
@@ -0,0 +1,22 @@
+﻿namespace TheCube.Models
+{
+    public class Card
+    {
+        public enum SUIT
+        {
+            CLUBS,
+            DIAMONDS,
+            HEARTS,
+            SPADES
+        }
+
+        public enum VALUE
+        {
+            TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+        }
+
+        public SUIT Myusuit {get; set;}
+
+        public VALUE MyValue { get; set;}
+    }
+}
